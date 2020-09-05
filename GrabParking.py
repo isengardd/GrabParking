aa2e4793621a1522672c6cbadbfde4a89eb2780e
isengardd <xuyao8598@msn.com>
2020-09-06 00:49:27 +0800

    最终代码提交

--- GrabParking.py
+++ GrabParking.py
@@ -7,6 +7,9 @@ import json
 import time
 import os
 import random
+import datetime
+import time
+from enum import Enum
 from urllib import request, parse
 import easyocr
 
@@ -16,7 +19,7 @@ LICENSE_PLATE = ""
 CACHE_FILE = "cache.txt"
 SESSION_KEY = "JSESSIONID"
 SAVE_SESSION_TIME = 2 * 3600 # session本地保存时间
-RELOGIN_TIME = 5 * 60 # 距离上次操作超过5分钟，重新登陆
+RELOGIN_TIME = 6 * 60 # 距离上次操作超过6分钟，重新登陆
 SVR_HOST = ""
 LOGIN_PAGE_URL = "{0}/pkt/login/loginPage".format(SVR_HOST)
 LOGIN_PROCESS_URL = "{0}/pkt/login/loginProcess".format(SVR_HOST)
@@ -27,6 +30,18 @@ MY_RENT_PAGE_URL = "{0}/pkt/service/myRentPage".format(SVR_HOST)
 PARKING_BOOK_URL = "{0}/pkt/service/parkingBooking".format(SVR_HOST)
 
 USER_AGENT = "Mozilla/5.0 (Linux; Android 8.0.0; MIX) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.81 Mobile Safari/537.36"
+
+class BookingState(Enum):
+  UNKNOWN = 0
+  ORDER_SUCCESS = 1
+  ORDER_NOT_OPEN = 2 #不在开放时段或者剩余车位为0
+  ORDER_EMPTY_PARKING = 3 #抢光了
+  IN_BLACK_LIST = 4 #被列入黑名单
+  ORDER_PREPARE = 5 #当前可抢
+  SESSION_ERROR = 6
+  NET_ERROR = 7
+  VERIFY_ERROR = 8 # 验证码错误
+  NO_FAIL = 9
 
 class CacheFileData:
   def __init__(self):
@@ -146,7 +161,7 @@ def Login():
 def GetReservePage():
   if GetSessionID() == "":
     print("GetReservePage failed: sessionID is empty")
-    return False
+    return BookingState.SESSION_ERROR
 
   CACHE_DATA.saveOpTime()
 
@@ -168,10 +183,10 @@ def GetReservePage():
       theHtmlPage = str(response.read(), "utf-8")
       if theHtmlPage.find("您已预约成功") != -1:
         print("已预约成功")
-        return False
+        return BookingState.ORDER_SUCCESS
       elif theHtmlPage.find("无法预约") != -1:
         print("当前无法预约")
-        return False
+        return BookingState.ORDER_NOT_OPEN
       else:
         # 定位车位数字信息
         searchCountStr = 'class="pkt-font" style="font-size: 30px; margin: 0; color: #ffffff">'
@@ -181,12 +196,12 @@ def GetReservePage():
           countStr = theHtmlPage[cutStart : cutStart + 2].split("/")
           if (len(countStr) > 0 and int(countStr[0]) == 0) or len(countStr) == 0:
             print("车位被抢光了")
-            return False
+            return BookingState.ORDER_EMPTY_PARKING
         else:
           print("无法定位剩余车位信息")
-          return False
-      return True
-  return False
+          return BookingState.UNKNOWN
+      return BookingState.ORDER_PREPARE
+  return BookingState.NET_ERROR
 
 def GetCode():
   if GetSessionID() == "":
@@ -221,6 +236,7 @@ def GetCode():
         verifyCode = ""
       else:
         # 识别成功，删除临时图片文件
+        print("verify code: {0}".format(verifyCode))
         os.remove(pngFilePath)
   return verifyCode
 
@@ -243,7 +259,7 @@ def QueryBlackList():
   req.add_header("Referer", RESERVE_PAGE_URL)
   req.add_header("User-Agent", USER_AGENT)
   req.add_header("X-Requested-With", "XMLHttpRequest")
-  with request.urlopen(req, data="") as response:
+  with request.urlopen(req, data="".encode("utf-8")) as response:
     print('QueryBlackList url Status:', response.status, response.reason)
     if response.status != 200:
       print("您已在黑名单中")
@@ -255,7 +271,7 @@ def QueryBlackList():
 def ParkBooking(verifyCode):
   if GetSessionID() == "":
     print("parkBooking failed: sessionID is empty")
-    return
+    return BookingState.SESSION_ERROR
 
   CACHE_DATA.saveOpTime()
 
@@ -273,10 +289,15 @@ def ParkBooking(verifyCode):
   req.add_header("Upgrade-Insecure-Requests", "1")
   req.add_header("User-Agent", USER_AGENT)
 
-  loginData = parse.urlencode([("user_id", 400), ("area_id", 3), ("license_plate", LICENSE_PLATE), ("booking_time", "tomorrow"), ("validateCode", verifyCode)]).encode("utf-8")
+  loginData = parse.urlencode([("user_id", 400), ("area_id", "3 "), ("license_plate", LICENSE_PLATE), ("booking_time", "tomorrow"), ("validateCode", verifyCode)]).encode("utf-8")
   with request.urlopen(req, loginData) as response:
     print('parkBooking Status:', response.status, response.reason)
-
+    if response.status == 302:
+      for k, v in response.getheaders():
+        if k == "Location" and v == "/pkt/service/reservePage?isSpare=3":
+          return BookingState.VERIFY_ERROR
+      return BookingState.NO_FAIL
+  return BookingState.NET_ERROR
 def MyRentPage():
   if GetSessionID() == "":
     print("parkBooking failed: sessionID is empty")
@@ -301,6 +322,30 @@ def MyRentPage():
 def Logout():
   pass
 
+def GrabParking():
+  #登录或者刷新session
+  Login()
+  tryCount = 0
+  while tryCount <= 2: # 最多尝试次数
+    tryCount += 1
+    #进入抢车位页面
+    bookingState = GetReservePage()
+    print("GetReservePage res = {0}".format(bookingState))
+    if bookingState == BookingState.ORDER_PREPARE:
+      # 获取验证码
+      verifyCode = GetCode()
+      if verifyCode != "":
+        # 查询是否在黑名单
+        if False == QueryBlackList():
+          bookingRes = ParkBooking(verifyCode)
+          if bookingRes == BookingState.VERIFY_ERROR:
+            # 如果是验证码错误，重新来一次
+            continue
+          # 这里应该就成功了，继续模拟一下后续的行为
+          MyRentPage()
+    break
+
+
 # 只识别英文
 reader = easyocr.Reader(['en'], gpu = False)
 if __name__ == "__main__":
@@ -309,7 +354,13 @@ if __name__ == "__main__":
   with open(cacheFilePath, "r+") as sessionFile:
     CACHE_DATA.loadFromJsonStr(sessionFile.read())
 
-  Login()
-  if GetReservePage() == True:
-    pass
-
+  while True:
+    # 只在周日~周4晚18：01运行一次，之后退出
+    nowTime = datetime.datetime.today()
+    weekday = nowTime.weekday()
+    if (weekday == 6 or (weekday >= 0 and weekday <= 3)) and \
+      nowTime.hour >= 18 and nowTime.minute >= 1:
+      GrabParking()
+      break
+    #每一秒判断一次
+    time.sleep(1)
